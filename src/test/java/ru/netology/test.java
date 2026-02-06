a6cce041cf12ebbc5db3b1dd94e7a919c1160f19
Kostya Yakovlev <akovlevkosta18@gmail.com>
2026-02-06 09:33:40 +0300

    Initial commit

---
+++ src/test/java/ru/netology/test.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void shouldStartTest() {
+        assertTrue(true);
+    }
+}
+
